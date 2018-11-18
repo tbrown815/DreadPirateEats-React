5e3d8cae0f4dce7fffc8b25472886b3deccf953f
BrotherFatcake <BrotherFatcake@brownscastle.com>
2018-11-17 19:02:16 -0600

    updated about section and added image of orig DPE

--- src/nav.js
+++ src/nav.js
@@ -72,9 +72,23 @@ export default class Nav extends React.Component {
                         <div>
                             <a href='#close' title='Close' className='close'>X</a>
                             <h2 className='modalTitle'>About Dread Pirate Eats!</h2>
-                            <p>This is how to play the Dread Pirate Eats game.</p>
+                            <h4>How to play the Dread Pirate Eats game-</h4>
                             <br />
-                            <p>This is the Dread Pirate Eats history.</p>
+                                <ol>
+                                    <li>Click the button to draw a resturant.</li>
+                                    <li>You receive 3 initial selections to choose from.</li>
+                                    <li>Your 4th selection is your "Last Stand", this is your final selection and if used is the option you must choose.</li>
+                                </ol>
+                            <h4>History of the Dread Pirate Eats-</h4>
+                            <p>The Dread Pirate Eats game started around 2009.  My wife and I found that either neither of us cared where we went 
+                                to eat OR one of us was feeling too picky with the others' suggestions.  Out of this issue the Dread Pirate Eats was born! <br/>
+                                We wrote the names down of our favorite and new resturants on pieces of paper, loaded them into an old pirate cookie jar, and 
+                                created the rules for the game.  Now the Dread Pirate Eats gets a digital upgrade!
+                            </p>
+                            <div className='origDPEBox'>
+                            <h4>The original Dread Pirate Eats!</h4>
+                            <img src={require('./origDPE.jpg')} alt='the original dread pirate eats' className='origDPE' />
+                            </div>
                         </div>
                     </div>
                 </header>
@@ -128,8 +142,12 @@ export default class Nav extends React.Component {
                         <div>
                             <a href='#close' title='Close' className='close'>X</a>
                             <h2 className='modalTitle'>About Dread Pirate Eats!</h2>
-                            <p>This is how to play the Dread Pirate Eats game.</p>
+                            <p>How to play the Dread Pirate Eats game.</p>
                             <br />
+                                <ol>
+                                    <li>Click the button to draw a resturant.</li>
+
+                                </ol>
                             <p>This is the Dread Pirate Eats history.</p>
                         </div>
                     </div>
