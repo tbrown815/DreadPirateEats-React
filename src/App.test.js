70490aea86313c3fd41173e44f60e24271ccb078
BrotherFatcake <BrotherFatcake@brownscastle.com>
2018-11-21 22:10:30 -0600

    test and package depend updates

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import DreadPirate from './App';
-//import {FIND_GRUB, RESTART_APP, LOGIN_USER, LOGOUT_USER, SIGNUP_USER, LOGIN_SUCCESS} from '../actions'
+//import {FIND_GRUB, RESTART_APP, LOGIN_USER, LOGOUT_USER, SIGNUP_USER, LOGIN_SUCCESS} from './actions'
 
-import reducer from '../reducer'
+import reducer from './reducer'
 
 describe('<DreadPirate />', () => {
 
     it('render without crashing', () => {
         shallow(<DreadPirate />);
     })
-}
+})
