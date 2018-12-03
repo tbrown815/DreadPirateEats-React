d43b6e79f2c27c1e20387c8a3e2e257e24050700
BrotherFatcake <BrotherFatcake@brownscastle.com>
2018-12-02 21:58:08 -0600

    updates to point to prod api

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
-
+/*
 exports.CREATE_USER_URL = 'http://localhost:8080/userAccess/new'
 exports.USER_LOGIN_URL = 'http://localhost:8080/userAuth/login'
+*/
+
+exports.CREATE_USER_URL = 'https://stark-badlands-90054.herokuapp.com/userAccess/new'
+exports.USER_LOGIN_URL = 'https://stark-badlands-90054.herokuapp.com/userAuth/login'
